a42fb0050fba43e0d8fee070451f2cfc4e562af9
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-08-07 20:39:32 -0400

    Added Keycloak Configuration

--- AlertNotificationApp/AlertNotificationApp.AppHost/AppHost.cs
+++ AlertNotificationApp/AlertNotificationApp.AppHost/AppHost.cs
@@ -1,11 +1,36 @@
+using Google.Protobuf.WellKnownTypes;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder.AddSqlite("db")    
                 .WithSqliteWeb();
 
+// configure Keycloak authentication
+var username = builder.AddParameter("keycloak-username", "admin")
+                      .WithDescription("Keycloak admin username");
+
+var password = builder.AddParameter("keycloak-password", "admin", secret: true);
+
+
+var keycloak = builder.AddKeycloak("keycloak", 8080, username, password)
+                      .WithDataVolume()
+                      .WithExternalHttpEndpoints();
+                      //.WithRealmImport("./KeycloakConfiguration");
 
 var apiService = builder.AddProject<Projects.WebApi>("webapi")
-                        .WithReference(db);
-                        
+                        .WithExternalHttpEndpoints()
+                        .WithReference(db)
+                        .WithReference(keycloak)
+                        .WaitFor(db)
+                        .WaitFor(keycloak)
+                        .WithEnvironment("Keycloak__ClientId", "confidential-client")
+                        .WithEnvironment("Keycloak__ClientSecret", "ze4SQDpbyBlB72kdTCTv8ecSWsJHf2Js");
+
+builder
+    .AddNpmApp("AngularFrontEnd", "../AlertNotificationWebApp")
+    .WithReference(apiService)
+    .WithEndpoint(4200, scheme: "http", env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
