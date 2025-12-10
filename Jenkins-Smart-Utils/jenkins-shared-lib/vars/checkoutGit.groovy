65efa31bf2a28324fea3b23b2611c36349d2f5df
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-12-10 18:58:37 -0500

    Checkout Git

--- Jenkins-Smart-Utils/jenkins-shared-lib/vars/checkoutGit.groovy
+++ Jenkins-Smart-Utils/jenkins-shared-lib/vars/checkoutGit.groovy
@@ -1,5 +1,7 @@
 def call(String repoUrl, String branch = 'main') {
-    sh """
-        git clone -b ${branch} ${repoUrl} .
-    """
+    // sh """
+    //     git clone -b ${branch} ${repoUrl} .
+    // """
+    git branch: '${branch}',
+                    url: '${repoUrl}'
 }
