d61769fcba14f61c928e81a045c72767d507927b
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-12-10 21:06:44 -0500

    Updated dotnetBuild.groovy with additional parameter

--- Jenkins-Smart-Utils/jenkins-shared-lib/vars/dotnetBuild.groovy
+++ Jenkins-Smart-Utils/jenkins-shared-lib/vars/dotnetBuild.groovy
@@ -1,8 +1,8 @@
 
-def call(String path){
+def call(String path, String projectFile) {
     sh """
         cd ${path}
-        dotnet restore
+        dotnet restore ${projectFile}
         dotnet build --configuration Release
         dotnet publish --configuration Release --output published
     """
