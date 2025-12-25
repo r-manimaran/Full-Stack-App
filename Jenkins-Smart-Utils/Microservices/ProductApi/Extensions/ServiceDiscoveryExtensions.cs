3ef014baec947a6ae0c908472a3f30bf29a1c7a9
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-12-24 20:20:37 -0500

    Added Logging for ServiceRegistration

--- Jenkins-Smart-Utils/Microservices/ProductApi/Extensions/ServiceDiscoveryExtensions.cs
+++ Jenkins-Smart-Utils/Microservices/ProductApi/Extensions/ServiceDiscoveryExtensions.cs
@@ -26,10 +26,27 @@ public static class ServiceDiscoveryExtensions
     private static void RegisterServiceDiscovery(IServiceCollection services, ServiceDiscoveryConfiguration discoveryConfig)
     {
         services.AddHostedService<ServiceDiscoveryHostedService>();
-        services.AddSingleton<IConsulClient, ConsulClient>(sp => new ConsulClient(consulConfig =>
+        services.AddSingleton<IConsulClient, ConsulClient>(sp =>
         {
+        var logger = sp.GetService<ILogger<Program>>();
+        logger?.LogInformation("Registering Consul client for service discovery."); 
+        var address = new Uri($"{discoveryConfig.ConsulConfiguration.Scheme}://{discoveryConfig.ConsulConfiguration.Host}:{discoveryConfig.ConsulConfiguration.Port}");
+
+        logger?.LogInformation("Creating Consul client pointing to {ConsulAddress}", address);
+        logger?.LogDebug("Service discovery config: ServiceName={ServiceName}, Host={Host}, Port={Port}, Version={Version}",
+            discoveryConfig.ServiceConfiguration.ServiceName,
+            discoveryConfig.ServiceConfiguration.Host,
+            discoveryConfig.ServiceConfiguration.Port,
+            discoveryConfig.ServiceConfiguration.Version);
+
+        return new ConsulClient(consulConfig =>
+        {
+            
             consulConfig.Address = new Uri($"{discoveryConfig.ConsulConfiguration.Scheme}://{discoveryConfig.ConsulConfiguration.Host}:{discoveryConfig.ConsulConfiguration.Port}");
-        }));
+            logger?.LogDebug("Consul client configured with address {Address}", consulConfig.Address);
+        });
+
+        });
     }
 }
 
