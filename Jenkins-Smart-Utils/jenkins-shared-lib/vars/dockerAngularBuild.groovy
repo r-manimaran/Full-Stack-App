8d380833d59b054d46da6f1ba17d96579e23aad9
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2026-01-24 12:51:19 -0500

    Fixed the Groovy file for angular docker build

--- Jenkins-Smart-Utils/jenkins-shared-lib/vars/dockerAngularBuild.groovy
+++ Jenkins-Smart-Utils/jenkins-shared-lib/vars/dockerAngularBuild.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String path){
     sh """
-        cd Jenkins-Smart-Utils/Microservices
+        cd Jenkins-Smart-Utils/MicroFrontends
         docker build -f ${path}/Dockerfile -t ${imageName}:latest .
     """
 }
